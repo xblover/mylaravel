6238a1be63fd7ec907f64114b6e9cbd68ddf4e25
xubo <xubo1395015060@qq.com>
2019-07-24 08:19:51 +0800

    first commit

---
+++ app/Http/Controllers/ShopProduct.php
@@ -0,0 +1,60 @@
+<?
+
+class ShopProduct {
+    private $title;
+    private $producerMainName;
+    private $producerFirstName;
+    protected $price;
+    private $discount = 0;
+
+    public function __construct($title, $firstName, $mainName, $price)
+    {
+        $this->title   = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName  = $mainName;
+        $this->price = $price;
+    }
+
+    public function getProducerFirstName()
+    {
+        return $this->producerFirstName();
+    }
+
+    public function getProducerMainName()
+    {
+        return $this->producerMainName();
+    }
+
+    public function setDiscount($num)
+    {
+        $this->discount = $num;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getPrice()
+    {
+        return ($this->price - $this->discount);
+    }
+
+    public function getProducer()
+    {
+        return "{$this->producerFirstName}"."{$this->producerMainName}";
+    }
+
+    public function getSummaryLine()
+    {
+        $base = "{$this->title} ({$this->producerMainName},";
+        $base .= "{$this->producerFirstName})";
+        return $base;
+    }
+
+}
